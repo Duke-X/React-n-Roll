43a228b8fbb536182ef473e5e7da883494ead1b3
Vadish Chhatwal <vadishchhatwal04@gmail.com>
2025-01-22 21:42:30 +0530

    Before Routing Method, basic skeleton

--- public/index.js
+++ public/index.js
@@ -3,23 +3,38 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
+const PORT = 3000;
 
-app.post('/signup', (req,res) => {
-
+app.post('/user/signup', (req,res) => {
+    res.json({
+        message : "Signup Endpoint"
+    })
 });
 
-app.post('/login', (req, res) => {
-
+app.post('/user/login', (req, res) => {
+    res.json({
+        message : "Login Endpoint"
+    })
 });
 
-app.post('/purchase', (req, res) => {
-
+app.post('/course/purchase', (req, res) => {
+    res.json({
+        message : "Buying Endpoint"
+    })
 });
 
-app.get('/all-courses', (req, res) => {
-
+app.get('/user/all-courses', (req, res) => {
+    res.json({
+        message : "All Course Endpoint"
+    })
 });
 
-app.get('/my-courses', (req, res) => {
-
+app.get('/user/my-courses', (req, res) => {
+    res.json({
+        message : "My Course Endpoint"
+    })
 });
+
+app.listen(PORT, {
+    message : console.log(`Server Running on port ${PORT}`)
+})
