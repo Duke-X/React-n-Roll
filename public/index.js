839a0102af5571d09d7bf10215b1f28cea77dada
Vadish Chhatwal <vadishchhatwal04@gmail.com>
2025-01-22 21:06:00 +0530

    Starting of CourseSellingBackend

---
+++ public/index.js
@@ -0,0 +1,25 @@
+//ALWAYS BUILD THE BACKEND FIRST THEN FRONTEND
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+app.post('/signup', (req,res) => {
+
+});
+
+app.post('/login', (req, res) => {
+
+});
+
+app.post('/purchase', (req, res) => {
+
+});
+
+app.get('/all-courses', (req, res) => {
+
+});
+
+app.get('/my-courses', (req, res) => {
+
+});
